45f56d6143e938ff024f26c357a70ae339618c84
Taehee <kairess87@gmail.com>
2021-02-27 22:46:16 +0900

    reference

--- KBHit.py
+++ KBHit.py
@@ -1,4 +1,5 @@
 '''
+https://stackoverflow.com/a/22085679/11157437
 A Python class implementing KBHIT, the standard keyboard-interrupt poller.
 Works transparently on Windows and Posix (Linux, Mac OS X).  Doesn't work
 with IDLE.
